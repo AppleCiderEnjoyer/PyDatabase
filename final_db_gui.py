e285b7d76d67a7055c7cf9ff11fc41a637ed124a
Briely Gunn <bcgunn@students.unwsp.edu>
2023-12-15 22:33:44 -0600

    I just realized we had an error if a user inputted to many rows (the GUI would become too long to fit on the screen), so I fixed it by adding a scroll bar to the window. Unfortunately, it messed up the formatting for the database items. If you guys could find time to help me fix it that would be great.

--- final_db_gui.py
+++ final_db_gui.py
@@ -9,6 +9,26 @@ GUI_text_justification = 20
 GUI_email_text_justification = 40
 
 
+# Functions
+# Function to justify all needed strings
+def gui_str_justification(string: str):
+    return string.ljust(GUI_text_justification, " ")
+
+
+# Function to format the database table into readable text
+def db_str_formatting(string):
+    string_formatted = (str(string)
+                        .replace("), ", "⌘")
+                        .replace("(", "")
+                        .replace(")", "")
+                        .replace("'", "")
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(",", "\t")
+                        .replace("⌘", ","))
+    return string_formatted
+
+
 # Intro GUI for when the program first runs
 class IntroGUI:
 
@@ -91,31 +111,37 @@ class DBGUI:
         self.exit_frame = tkinter.Frame(self.window)
 
         # Text
-        self.intro_text = tkinter.Label(self.text_frame,
-                                        text="Student Information Database\n")
+        self.intro_text = tkinter.Label(self.db_frame, text="Student Information Database\n")
 
         # Database text
         # Formats the database columns
-        self.db_text = tkinter.Label(self.db_frame,
-                                     text=f"ID#:"
-                                          "\t Name:".ljust(GUI_text_justification, " ") +
-                                          "\t Age:"
-                                          "\t Gender:"
-                                          "\t Country:".ljust(GUI_text_justification, " ") +
-                                          "\t\t Timezone:".ljust(GUI_text_justification, " ") +
-                                          "\t Email:\n".ljust(GUI_email_text_justification, " "),
-                                     justify='left',
-                                     anchor='w', width=100)
+        self.db_text = ("ID#:".ljust(GUI_text_justification, " ") +
+                        "\t Name:".ljust(GUI_text_justification, " ") +
+                        "\t Age:" +
+                        "\t Gender:" +
+                        "\t Country:".ljust(GUI_text_justification, " ") +
+                        "\t\t Timezone:".ljust(GUI_text_justification, " ") +
+                        "\t Email:\n".ljust(GUI_email_text_justification, " "))
 
         # Formats the Database info
-        self.db_info = str(final_db_functions.get_students())
-        self.db_info_formatted = self.db_str_formatting(self.db_info)
+        self.db_entries = final_db_functions.get_students()
 
-        # Adds the formatted database information to the label
-        self.db_entries = tkinter.Label(self.db_frame,
-                                        text=self.db_info_formatted,
-                                        justify='left',
-                                        anchor='w', width=100)
+        #
+        self.db_scrollbox = tkinter.Listbox(self.db_frame,
+                                            justify='left',
+                                            height=10,
+                                            width=100)
+        #
+        self.db_scrollbar = tkinter.Scrollbar(self.db_frame,
+                                              orient=tkinter.VERTICAL)
+
+        #
+        self.db_scrollbar.config(command=self.db_scrollbox.yview)
+        self.db_scrollbox.config(yscrollcommand=self.db_scrollbar.set)
+
+        # Adds the database entries to the scrollbox
+        self.db_scrollbox.insert(tkinter.END, self.db_text)
+        self.db_gui_formatting(self.db_entries)
 
         # Buttons
         # Creates a button to edit info into the database
@@ -138,11 +164,12 @@ class DBGUI:
 
         # Packing
         # Adds the text to the GUI
-        self.intro_text.pack()
+        self.intro_text.pack(side='top')
 
         # Adds the database information to the GUI
-        self.db_text.pack()
-        self.db_entries.pack()
+        # self.db_entries.pack()
+        self.db_scrollbox.pack(side='left')
+        self.db_scrollbar.pack(side='right', fill=tkinter.Y)
 
         # Adds the buttons to the GUI
         self.edit_db_entry_button.pack(side='left')
@@ -153,7 +180,7 @@ class DBGUI:
         # Frame packing
         # Adds the frames to the GUI
         self.text_frame.pack()
-        self.db_frame.pack()
+        self.db_frame.pack(padx=20)
         self.button_frame.pack()
         self.exit_frame.pack()
 
@@ -270,17 +297,34 @@ class DBGUI:
         # Mainloop to make the GUI work
         tkinter.mainloop()
 
-    # Method to format the database table into readable text
-    def db_str_formatting(self, db_string):
-        self.db_info_formatted = (str(db_string)
-                                  .replace("), ", "\n")
-                                  .replace("(", "")
-                                  .replace(")", "")
-                                  .replace("'", "")
-                                  .replace("[", "")
-                                  .replace("]", "")
-                                  .replace(",", "\t"))
-        return self.db_info_formatted
+    # Method to format database entries for the scrollbox
+    def db_gui_formatting(self, db_info: list):
+
+        for row in db_info:
+
+            # Gets the fields in the selected row
+            db_id = str(row[0])
+            db_name = str(row[1])
+            db_age = str(row[2])
+            db_gender = str(row[3])
+            db_state = str(row[4])
+            db_timezone = str(row[5])
+            db_email = str(row[6])
+
+            db_str_formatting(db_id)
+            db_str_formatting(db_name)
+            db_str_formatting(db_age)
+            db_str_formatting(db_gender)
+
+            db_row = (f"{db_id}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_name}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_age}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_gender}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_state}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_timezone}".ljust(GUI_text_justification, " ") +
+                      f"\t{db_email}")
+
+            self.db_scrollbox.insert(tkinter.END, db_row)
 
     # Coded by: Briely Gunn
     # Method to add a student to the database and print it to the GUI when the "Add Student" button is clicked
@@ -300,25 +344,24 @@ class DBGUI:
                                        str(self.add_email_textbox.get().ljust(GUI_email_text_justification, " ")))
 
         # Gets the updated database info and formats it
-        self.db_info = str(final_db_functions.get_students())
-        self.db_info_formatted = self.db_str_formatting(self.db_info)
+        self.db_entries = str(final_db_functions.get_students()[final_db_functions.get_students().__len__() - 1])
+        self.db_entries = db_str_formatting(self.db_entries)
+        # self.db_info_formatted = self.db_str_formatting(self.db_info)
 
         # Destroys the erroneous database
-        self.db_entries.destroy()
+        # self.db_entries.destroy()
 
         # Adds the formatted database info to the GUI
-        self.db_entries = tkinter.Label(self.db_frame,
-                                        text=self.db_info_formatted,
-                                        justify='left',
-                                        anchor='w', width=100)
+        self.db_gui_formatting(final_db_functions.get_students())
 
         # Packs the database info to the GUI
-        self.db_entries.pack()
 
         # Unpacks the add_db_entry_frame for a cleaner GUI
         self.add_db_entry_frame.pack_forget()
+        self.db_frame.pack_forget()
 
         # Repacks the exit and button frames
+        self.db_frame.pack()
         self.button_frame.pack()
         self.exit_frame.pack()
 
