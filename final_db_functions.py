4cde3f39083d41c9710866498335fdf91666135f
cdhard15 <153309417+cdhard15@users.noreply.github.com>
2023-12-16 21:45:46 -0600

    Update final_db_functions.py
    fixed remove_student function

--- final_db_functions.py
+++ final_db_functions.py
@@ -83,11 +83,9 @@ def edit_student(student_id: int, updated_values: dict):
 def remove_student(student_id: int):
     # Uses variables to connect to the database
     cursor, connection = get_connection()
-
-    delete_id = input(int("Enter student id for student you want to remove: "))
-    for row in cursor.execute('SELECT student_id FROM students'):
-        if student_id == delete_id:
-            cursor.execute('DELETE FROM students WHERE student_id = delete_id')
+    
+    cursor.execute("DELETE FROM students WHERE student_id = ' + str(student_id)'")
+    
     connection.commit()
     connection.close()
     
